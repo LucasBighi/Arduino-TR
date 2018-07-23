37a22397df5aa2e90ceab0121bc40f5e177f1776
Lucas Marques Bighi <lucasbighi@air-de-lucas>
2018-07-23 17:31:00 -0300

    Initial Commit

---
+++ Arduino TR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Arduino TR
+//
+//  Created by Lucas Marques Bighi on 23/07/2018.
+//  Copyright © 2018 Lucas Marques Bighi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
